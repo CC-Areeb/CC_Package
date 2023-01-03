a0526f24727ae41143ee781c158c10be7f7a35ea
Areeb Malik <96907614+CC-Areeb@users.noreply.github.com>
2023-01-03 12:38:37 +0500

    Update SMSController.php

--- package/CC-SMS/src/Http/Controllers/SMSController.php
+++ package/CC-SMS/src/Http/Controllers/SMSController.php
@@ -14,22 +14,36 @@ class SMSController extends Controller
         return view('sms-index');
     }
 
-    public function sendSMS(Request $request)
+    public function sendSMS()
     {
+        $otp = $this->generateOTP();
+
+        $request = (object) [
+            'sms_message' => 'Text message with OTP: '. $otp,
+            'sms_receiver' => '<The registered phone number(s)>',
+        ];
+
         try {
             $sid = config('sms.sid', 'some_sid');
             $sender = config('sms.sender', 'some_sender');
             $authToken = config('sms.auth', 'some_token');
             $twilio = new Client($sid, $authToken);
-            $twilio->messages->create($request->sms_receiver, [
+            $twilio->messages->create(
+                $request->sms_receiver,
+                [
                     "body" => $request->sms_message,
                     "from" => $sender,
                     "mediaUrl" => ["https://demo.twilio.com/owl.png"]
                 ]
             );
-            return view('sms-welcome');
-        }catch (TwilioException $e) {
-            return 'Oops, SMS was not sent.';
+            return 'SMS was sent';
+        } catch (TwilioException $e) {
+            return 'Oops, SMS was not sent due to. ' . '<br /> <br />' . $e;
         }
+    }
+
+    public function generateOTP()
+    {
+        return random_int(0,99999);
     }
 }
