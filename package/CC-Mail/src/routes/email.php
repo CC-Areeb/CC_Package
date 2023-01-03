7c64f03a318e6a53f474f1984928d260deb4f07f
Areeb Malik <96907614+CC-Areeb@users.noreply.github.com>
2023-01-03 18:30:20 +0500

    Bugs

--- package/CC-Mail/src/routes/email.php
+++ package/CC-Mail/src/routes/email.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\EmailController;
 use Illuminate\Support\Facades\Route;
 
-dump('vendor');
-
 Route::prefix('email')->group(function () {
     Route::get('/', [EmailController::class, 'index'])->name('index');
     Route::post('/send', [EmailController::class, 'sendEmail'])->name('sendEmail');
